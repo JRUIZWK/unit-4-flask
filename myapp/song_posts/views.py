8b5e0eb4846d8b19f9cf50107a5087861fff319d
jruiz <jruiz.wk@gmail.com>
2022-04-13 17:02:17 -0500

    Add update functionality to song-posts

--- myapp/song_posts/views.py
+++ myapp/song_posts/views.py
@@ -20,6 +20,7 @@ def create_post():
     return redirect(url_for('core.index'))
   return render_template('create_post.html', form=form)
 
+
 @song_posts.route('/<int: song_post_id>')
 def song_post(song_post_id):
   song_post = FavSongs.query.get_or_404(song_post_id)
@@ -48,3 +49,17 @@ def update(song_post_id):
         form.text.data = song_post.text
 
     return render_template('create_post.html',title='Updating',form=form)
+
+
+@song_posts.route('/<int:song_post_id>/delete',methods=['GET','POST'])
+@login_required
+def delete_post(song_post_id):
+
+    song_post = FavSongs.query.get_or_404(song_post_id)
+    if song_post.author != current_user:
+        abort(403)
+
+    db.session.delete(song_post)
+    db.session.commit()
+    flash('Song Post Deleted')
+    return redirect(url_for('core.index'))
