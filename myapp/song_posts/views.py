b8cd4cd3b4b0eb389c5157be111399bc94731b1e
jruiz <jruiz.wk@gmail.com>
2022-04-13 16:53:33 -0500

    Add delete functionality to song-posts

--- myapp/song_posts/views.py
+++ myapp/song_posts/views.py
@@ -24,3 +24,27 @@ def create_post():
 def song_post(song_post_id):
   song_post = FavSongs.query.get_or_404(song_post_id)
   return render_template('song_post.html', title=song_post.title, date=song_post.date, post=song_post)
+
+
+@song_posts.route('/<int:song_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(song_post_id):
+    song_post = FavSongs.query.get_or_404(song_post_id)
+
+    if song_post.author != current_user:
+        abort(403)
+
+    form = FavSongsForm()
+
+    if form.validate_on_submit():
+        song_post.title = form.title.data
+        song_post.text = form.text.data
+        db.session.commit()
+        flash('Song Post Updated')
+        return redirect(url_for('song_posts.song_post',song_post_id=song_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = song_post.title
+        form.text.data = song_post.text
+
+    return render_template('create_post.html',title='Updating',form=form)
